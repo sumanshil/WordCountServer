721513e7b7ae51dab728b5ca4c9b244452dc42bc
sshil <P@ssw0rd>
2017-09-12 09:02:50 -0700

    minor change

--- src/main/java/com/org/data/Result.java
+++ src/main/java/com/org/data/Result.java
@@ -5,6 +5,7 @@ import com.org.threadpool.ThreadPool;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Result {
     private static Result resultContainer = new Result();
@@ -20,16 +21,16 @@ public class Result {
 
     private static Map<String, String> map = new ConcurrentHashMap<>();
 
-    private int currentUnique = 0;
-    private int currentDuplicate = 0;
-    private int totalUnique = 0;
+    private AtomicInteger currentUnique = new AtomicInteger(0);
+    private AtomicInteger currentDuplicate = new AtomicInteger(0);
+    private AtomicInteger totalUnique  = new AtomicInteger(0);
 
     public void add(String value) {
         if (map.putIfAbsent(value, value) != null) {
-            currentDuplicate++;
+            currentDuplicate.incrementAndGet();
         } else {
-            currentUnique++;
-            totalUnique++;
+            currentUnique.incrementAndGet();
+            totalUnique.incrementAndGet();
             map.put(value, value);
             ThreadPool.getInstance().getPool().submit(new FileWriter(value));
         }
@@ -59,10 +60,10 @@ public class Result {
         }
     }
 
-    public ResultObject getResultObject(){
-        ResultObject retVal =  new ResultObject(currentUnique, currentDuplicate, totalUnique);
-        currentUnique = 0;
-        currentDuplicate = 0;
+    public ResultObject getResultObject() {
+        ResultObject retVal =  new ResultObject(currentUnique.get(), currentDuplicate.get(), totalUnique.get());
+        currentUnique = new AtomicInteger(0);
+        currentDuplicate = new AtomicInteger(0);
         return retVal;
     }
 }
